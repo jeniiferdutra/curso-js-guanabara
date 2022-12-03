adee94e3662d8e097f0c9add3c191ec74943f68a
Jenifer <jeniferdutra2000@icloud.com>
2022-12-03 01:51:22 -0300

    primeiro-commit

---
+++ ex009.js
@@ -0,0 +1,6 @@
+let vel = 78.3
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) {
+    console.log('Voce ultrapassou a velocidade permitida')
+}
+console.log('Dirija sempre com segurança!!')
