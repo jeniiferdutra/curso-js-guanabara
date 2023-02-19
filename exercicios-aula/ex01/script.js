f0b7540f41261bd46e5347186853fe482030ad2d
Jenifer <jeniferdutra2000@icloud.com>
2023-02-19 14:34:14 -0300

    retomando

--- exercicios-aula/ex01/script.js
+++ exercicios-aula/ex01/script.js
@@ -1,17 +1,17 @@
-function carregar(){
+function carregar() {
     var msg = document.querySelector('#msg')
-    var imagem = document.querySelector('.foto')
+    var img = document.querySelector('#imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
-    if(hora >= 0 && hora < 12){
-        imagem.innerHTML = '<img src ="img/manha.jpg">'
-        document.body.style.background = '#D0BF92'
-    } else if(hora >= 12 && hora < 18){
-        imagem.innerHTML = '<img src="img/tarde.jpg">'
-        document.body.style.background = '#A57662'
+    if(hora >= 0 && hora < 12) {
+        img.src = 'img/manha.jpg'
+        document.body.style.background = '#DEBA77'
+    } else if(hora >= 12 && hora <= 18){
+        img.src = 'img/tarde.jpg'
+        document.body.style.background = '#D87F2D'
     } else {
-        imagem.innerHTML = '<img src="img/noite.jpg">'
-        document.body.style.background = '#464748'
+        img.src = 'img/noite.jpg'
+        document.body.style.background = '#404C5A'
     }
 }
