608bbdce54d7e066ddeb61e4ac61f749bc490701
Jenifer <jeniferdutra2000@icloud.com>
2023-02-20 15:40:07 -0300

    retomando

--- exercicios-aula/ex03/script.js
+++ exercicios-aula/ex03/script.js
@@ -1,32 +1,36 @@
-
-function contar(){
-    var i = document.querySelector('#ini')
-    var f = document.querySelector('#f')
-    var inc = document.querySelector('#pass')
-    var res = document.querySelector('.msg')
-
-    if(i.value.length == 0 || f.value.length == 0 || inc.value.length == 0) {
-        window.alert('[ERRO] Faltam dados')
+function contar() {
+    var ini = document.querySelector('#n1')
+    var fim = document.querySelector('#n2')
+    var passo = document.querySelector('#pass')
+    var res = document.querySelector('.res')
+    
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
     } else {
         res.innerHTML = 'Contando: <br>'
-        var c = Number(i.value)
-        var fi = Number(f.value)
-        var cr = Number(inc.value)
-        if(cr <= 0){
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0) {
             alert('Passo inválido! Considerando PASSO 1')
-            cr = 1
+            p = 1
         }
 
-        if(c < fi){
-            for(var cont = c; cont <= fi; cont += cr) {
-            res.innerHTML += ` ${cont} \u{1F449}`
+        if(i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
             }
-        }  else {
-            for(var cont = c; cont >= fi; cont -= cr) {
-                res.innerHTML += ` ${cont} \u{1F449}`
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
             }
         }
-        res.innerHTML += `\u{1F3F3}`
-    }
-  
+        res.innerHTML += `\u{1F3C1}`
+    }        
 }
+
+
+
