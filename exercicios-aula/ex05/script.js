af02fd44dc38fd646ac65d3b037668eceed2aa85
Jenifer <jeniferdutra2000@icloud.com>
2023-03-20 08:54:55 -0300

    retomando

--- exercicios-aula/ex05/script.js
+++ exercicios-aula/ex05/script.js
@@ -1,9 +1,9 @@
-let num = document.querySelector('#txt1')
-let lista = document.querySelector('#formulario')
-let res = document.querySelector('#res')
-let vetor = []
+let num = document.querySelector('#num')
+let res = document.querySelector('.res')
+let lista = document.querySelector('#lista')
+let vet = []
 
-function isNumber(n) {
+function isNumero(n) {
     if(Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
@@ -11,52 +11,18 @@ function isNumber(n) {
     }
 }
 
-function inList(n, l) {
-    if(l.indexOf(Number(n)) != -1) {
-        return true
-    } else {
-        return false
-    }
-}
+
 
 function adicionar() {
-    if(isNumber(num.value) && !inList(num.value, vetor)) {
-        vetor.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado`
-        lista.appendChild(item)
-        res.innerHTML = ''
-    } else {
-        alert('Número inválido ou já encontrado na lista.')
-    }
-    num.value = ''
-    num.focus()
-}
 
-function finalizar() {
-    if(vetor.length == 0) {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
         alert('Adicione valores antes de finalizar')
     } else {
-        let tot = vetor.length
-        let maior = vetor[0]
-        let menor = vetor[0]
-        let soma = 0
-        let media = 0
-        for(let pos in vetor) {
-            soma += vetor[pos]
-            if(vetor[pos] > maior) {
-                maior = vetor[pos]
-            }
-            if(vetor[pos] < menor) {
-                menor = vetor[pos]
-            }
-        }
-        media = soma / tot
+        let n = Number(num.value)
         res.innerHTML = ''
-        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
-        res.innerHTML +=`<p>O maior valor encontrado foi ${maior}</p>`
-        res.innerHTML += `<p>O menor valor encontrado foi ${menor}</p>`
-        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
-        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+        vet = [n]
+        if(vet.indexOf != -1) {
+            res.innerHTML += `Valor ${n} adicionado`
+        }
     }
 }
